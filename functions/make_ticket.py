7b1519d88425a007918133a3df93448fe2f08fdb
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-07-15 03:31:12 +0300

    Update make_ticket.py

--- functions/make_ticket.py
+++ functions/make_ticket.py
@@ -5,4 +5,4 @@ class Ticket:
     def make_ticket_prime(self, user_id):
         numbers = str(random.randint(0, 9999)).zfill(4)
         password = str(user_id)
-        self.password = password+numbers
+        self.password = int(password+numbers)
