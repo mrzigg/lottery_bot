a3d8005244d64138dec8bead9c895e46f9545898
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-07-21 02:20:33 +0300

    Delete follower_board.py

--- menu/inline/follower_board.py
+++
@@ -1,6 +0,0 @@
-from aiogram import types 
-
-
-button_1 = types.InlineKeyboardButton("Подписаться✅", url="https://t.me/TELEGIV_TEST")
-button_2 = types.InlineKeyboardButton("🔎Проверить подписку", callback_data="check_in")
-keyboard = types.InlineKeyboardMarkup(row_width=1).add(button_1, button_2)
