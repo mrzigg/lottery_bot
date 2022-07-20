c6dab63a0b4a1cab704430ff532d2b11d11ccf03
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-07-21 02:13:53 +0300

    some updates

--- menu/reply/start_board.py
+++ menu/reply/start_board.py
@@ -1,5 +1,5 @@
-from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-start_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-button = types.KeyboardButton("Начать🔥")
-start_menu.add(button)
+
+button = KeyboardButton("Начать🔥")
+start_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button)
--- menu/reply/user_menu.py
+++ menu/reply/user_menu.py
@@ -1,8 +1,7 @@
-from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-button_1 = types.KeyboardButton("🎫 Мои билеты")
-button_2 = types.KeyboardButton("⏰ Когда закончится розыгрыш")
-button_3 = types.KeyboardButton("🎁 Бонусные билеты")
 
-keyboard.add(button_1).add(button_2).add(button_3)
+button_1 = KeyboardButton("🎫 Мои билеты")
+button_2 = KeyboardButton("⏰ Когда закончится розыгрыш")
+button_3 = KeyboardButton("🎁 Бонусные билеты")
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(button_1).add(button_2).add(button_3)
