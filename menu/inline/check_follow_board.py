9748da055eff5d72d188bdcfab55df96b954d965
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-07-22 05:00:27 +0300

    Add files via upload

--- menu/inline/check_follow_board.py
+++ menu/inline/check_follow_board.py
@@ -7,11 +7,8 @@ Participation_Menu = InlineKeyboardMarkup(row_width=1).add(participation)
 subscription1 = KeyboardButton(text="Да, давай исправим😀", callback_data="lets_fix")
 Subscription_Menu1 = InlineKeyboardMarkup(row_width=1).add(subscription1)
 
-subscription = KeyboardButton(text="Подписаться✅", url="https://t.me/TELEGIV_TEST") 
 check_subscription = KeyboardButton(text="Проверить подписку🔎", callback_data="check_subscription")
-Subscription_Menu = InlineKeyboardMarkup(row_width=1).add(subscription, check_subscription)
+Subscription_Menu = InlineKeyboardMarkup(row_width=1).add(check_subscription)
 
 check_in = KeyboardButton("🔎Проверить подписку", callback_data="check_in")
-Subscription_Menu_2 = InlineKeyboardMarkup(row_width=1).add(subscription, check_in)
-
-Subscription_Menu_3 = InlineKeyboardMarkup(row_width=1).add(subscription)
+Subscription_Menu_2 = InlineKeyboardMarkup(row_width=1).add(check_in)
