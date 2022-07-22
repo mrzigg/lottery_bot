66f55864677fee44b94ad71ae779cf0d8bbdb47e
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-07-22 04:59:56 +0300

    Add files via upload

---
+++ functions/make_links.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from filters.channels import CHANNELS
+
+class Channel_link:
+
+    def make_links(self):
+        links = ""
+        for row in CHANNELS:
+            links += f"👉 @{row}\n"
+        self.links = links
--- functions/make_ticket.py
+++ functions/make_ticket.py
@@ -13,16 +13,15 @@ import database.tickets_db as ticket_db
 
 class Ticket:
 
-    async def make_ticket_prime(self, user_id):
+    def make_ticket_prime(self, user_id):
         numbers = str(randint(0, 9999)).zfill(4)
-        password = str(user_id)
-        self.password = int(password+numbers)
+        self.password = int(str(user_id)+numbers)
 
     async def updating_db(self, user_id, counter):
         ticket = await ticket_db.get_tickets(user_id)
         self.ticket_sp = list()
         for i in range(counter):
-            await self.make_ticket_prime(user_id) 
+            self.make_ticket_prime(user_id) 
             ticket.append(self.password)
             self.ticket_sp.append(str(self.password))
         await ticket_db.update_function(user_id, ticket, "tickets")
