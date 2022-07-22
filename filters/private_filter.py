15aa70808c6f1b7d4855b071649409eeb23a8ed5
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-07-22 04:59:35 +0300

    Add files via upload

--- filters/private_filter.py
+++ filters/private_filter.py
@@ -1,6 +1,8 @@
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
 
+from .channels import CHANNELS
+
 import os
 import sys
 import inspect
@@ -9,14 +11,24 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from menu.inline.check_follow_board import Subscription_Menu_3
 from config.load_all import bot
 
 class PrivateFilter(BoundFilter):
     async def check(self, message: types.Message):
-        user_channel_status = await bot.get_chat_member(chat_id=-1001576599264, user_id=message.from_user.id)
-        if user_channel_status["status"] != 'left':
+        status = bool(True)
+        user_status = bool(False)
+        for row in CHANNELS:
+            user_channel_status = await bot.get_chat_member(chat_id=f"@{row}", user_id=message.from_user.id)
+            if user_channel_status['status'] == "left":
+                user_status = bool(False)
+            else:
+                user_status = bool(True)
+            status = bool(status*user_status)
+        if status is True:
             return True
         else:
-            await bot.send_message(message.from_user.id, '<b>Вы не подписаны на канал❌</b>', reply_markup=Subscription_Menu_3, disable_web_page_preview=True)
+            links = ""
+            for row in CHANNELS:
+                links += f"@{row}\n"
+            await bot.send_message(message.from_user.id, f'<b>Вы не подписаны на канал❌</b>\n{links}')
             return None
