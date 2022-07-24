48b13cf1fe939a73446dfce483252f04445d0b20
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-07-24 04:47:30 +0300

    Add files via upload

--- filters/channels.py
+++ filters/channels.py
@@ -1,1 +1,11 @@
-CHANNELS = ["TELEGIV_TEST", "TEST_TELEGIV_2"]
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from variables import CHANNELS
+
+CHANNELS = CHANNELS
