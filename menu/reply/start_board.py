6fe9d7456496113c4535c12dc8669ef50f7fdac6
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 23:56:06 +0300

    Participation keyboard

--- menu/reply/start_board.py
+++ menu/reply/start_board.py
@@ -1,5 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-button = KeyboardButton("Начать🔥")
+button = KeyboardButton("Начать 🔥")
 start_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button)
