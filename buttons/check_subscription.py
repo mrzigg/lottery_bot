e7221221819d6d07f6fac7c62b147643109fd9c4
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:01:26 +0300

    Getting the user's gender
    Also checking for a subscription

--- buttons/check_subscription.py
+++ buttons/check_subscription.py
@@ -8,13 +8,14 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from config.load_all import dp, bot
-from filters.during_sub import DuringSub
 import database.users_db as db
 import database.tickets_db as ticket_db
-from menu.inline.gender_board import gender_board
+
+from config.load_all import dp, bot
+from filters.during_sub import DuringSub
 from menu.inline.age_board import age_board
 from functions.message_routins import Routins
+from menu.inline.gender_board import gender_board
 
 
 @dp.callback_query_handler(DuringSub(), text="check_in")
@@ -22,8 +23,8 @@ async def callback_check_in(callback_query: types.CallbackQuery):
     await db.update_button_2(callback_query.from_user.id)
     ticket = await ticket_db.add_user(callback_query.from_user.id)
     await bot.send_message(callback_query.from_user.id, f"<b>Отлично!</b>\nРобот всё проверил - всё отлично)\n\n<b>Теперь ты участник розыгрыша!</b>\n\n🎫 Номер лотерейного билета: <b>{ticket}</b>", reply_markup=types.ReplyKeyboardRemove())
-    await Routins.routin_callback(callback_query=callback_query)
-    await bot.send_message(callback_query.from_user.id, "Кстати, ты можешь получить <b>+1 лотерейный билет!</b>\n\n<b>Просто ответь на вопрос:</b> Ты парень или девушка?\n\n<i>P.S.Твои ответы позволят нам понять, как в следующий раз сделать розыгрыш еще интереснее)</i>",
+    await Routins.routin_callback(callback_query=callback_query) 
+    return await bot.send_message(callback_query.from_user.id, "Кстати, ты можешь получить <b>+1 лотерейный билет!</b>\n\n<b>Просто ответь на вопрос:</b> Ты парень или девушка?\n\n<i>P.S.Твои ответы позволят нам понять, как в следующий раз сделать розыгрыш еще интереснее)</i>",
     reply_markup=gender_board)
 
 
@@ -34,6 +35,6 @@ async def gender_message(callback_query: types.CallbackQuery):
     else:
         await db.update_gender(callback_query.from_user.id, False)
     ticket = await ticket_db.add_user(callback_query.from_user.id)
-    await bot.send_message(callback_query.from_user.id, f"<b>Воу, это круто ☺️</b>\n\nДержи +1 лотерейный билет\n🎫 Номер:<u> {ticket} </u>\n\nТеперь у тебя <b>2</b> лотерейных билета.\n\nОтветь на вопрос - получи еще <b>+1</b>.", reply_markup=types.ReplyKeyboardRemove())
+    await bot.send_message(callback_query.from_user.id, f"<b>Воу, это круто ☺️</b>\n\nДержи +1 лотерейный билет\n🎫 Номер: {ticket}\n\nТеперь у тебя <b>2</b> лотерейных билета.\n\nОтветь на вопрос - получи еще <b>+1</b>.", reply_markup=types.ReplyKeyboardRemove())
     await Routins.routin_callback(callback_query=callback_query)
-    await bot.send_message(callback_query.from_user.id, "<b>Сколько тебе лет?</b>", reply_markup=age_board)
+    return await bot.send_message(callback_query.from_user.id, "<b>Сколько тебе лет?</b>", reply_markup=age_board)
