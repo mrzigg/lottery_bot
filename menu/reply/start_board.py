e8d43967148201cda81bfff091c3d0efac17c2be
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:51:31 +0300

    Delete start_board.py

--- menu/reply/start_board.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
-button = KeyboardButton("Начать🔥")
-start_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button)
