ba233fcae07384acdde436ed225b8478ed523ddb
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:11:54 +0300

    Routine functions

--- buttons/sub_during.py
+++ buttons/sub_during.py
@@ -10,16 +10,17 @@ sys.path.insert(0, parentdir)
 
 from config.load_all import dp
 from filters.during_sub import DuringSub2
-from buttons.check_subscription import callback_check_in
 from functions.message_routins import Routins
+from buttons.check_subscription import callback_check_in
 
 
 @dp.callback_query_handler(DuringSub2(), text='during_sub')
 async def during_sub_check(call: types.CallbackQuery):
     await Routins.edit_callback(call=call)
-    await callback_check_in(callback_query=call)
+    return await callback_check_in(callback_query=call)
+
 
 @dp.callback_query_handler(text='during_sub_2')
 async def during_sub_check(call: types.CallbackQuery):
     await call.answer()
-    await callback_check_in(callback_query=call)
+    return await callback_check_in(callback_query=call)
