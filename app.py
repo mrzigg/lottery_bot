b2dbfe3514893ea33a5e1938f1a52765a56e97d4
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:58:41 +0300

    File for launching the bot

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 from aiogram.utils.executor import start_webhook
+from datetime import timedelta
 import logging
 
+import config.load_all as cfg
 from variables import end_timestamp
 from config.load_all import dp, scheduler
-import config.load_all as cfg
 
 import commands.start
-import commands.channels
 import commands.raffle
-import buttons.start_play
-import buttons.i_am_in
-import buttons.my_tickets
-import buttons.check_subscription
-import buttons.sub_during
+import commands.channels
+
 import buttons.age
 import buttons.country
-import buttons.bonus_tickets
+import buttons.i_am_in
+import buttons.start_play
+import buttons.my_tickets
+import buttons.sub_during
 import buttons.ending_time
-
-from functions.sending_message import *
+import buttons.bonus_tickets
+import buttons.check_subscription
+import buttons.super_game_third_stage
+import buttons.super_game_second_stage
+import buttons.super_game_fourth_stage
 from buttons.win_fuction import find_winner_function
 
+from functions.today import *
+from functions.tomorrow import *
+from functions.three_days import *
+from functions.sending_message import *
+
 logging.basicConfig(format=u'%(filename)+13s [LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s', level=logging.INFO)
+
 
 async def on_startup(dp):
     await bot.set_webhook(cfg.WEBHOOK_URL)
@@ -30,13 +39,17 @@ async def on_startup(dp):
     await cfg.db.create_pool()
     logging.warning("Database connection is opened")
     scheduler.add_job(find_winner_function, run_date=end_timestamp)
-    scheduler.add_job(sending_message, "interval", days=1)
+    scheduler.add_job(sending_message, "interval", days=1, end_date=(end_timestamp-timedelta(days=4)))
+    scheduler.add_job(sending_message_three_days, "date", run_date=(end_timestamp-timedelta(days=3)))
+    scheduler.add_job(sending_message, "date", run_date=(end_timestamp-timedelta(days=2)))
+    scheduler.add_job(sending_message_tomorrow, "date", run_date=(end_timestamp-timedelta(days=1)))
+    scheduler.add_job(sending_message_today, "date", run_date=(end_timestamp-timedelta(hours=12)))
     logging.warning("Sending tasks is active")
 
 
 async def on_shutdown(dp):
     logging.warning('Shutting down...')
-    await bot.delete_webhook()
+    #await bot.delete_webhook()
     logging.warning('Bye!')
     scheduler.shutdown()
     logging.warning("AsyncIOScheduler is anactive")
