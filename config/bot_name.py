bae4d40ac81a508003f004960f7f224de41a7a0e
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:22:04 +0300

    Bot's name

---
+++ config/bot_name.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from variables import bot_name
+
+
+link = f"https://t.me/{bot_name}?start="
