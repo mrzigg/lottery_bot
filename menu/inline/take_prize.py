70c537160208069b6cad4488800c4dee8abfd6e8
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:45:37 +0300

    Delete take_prize.py

--- menu/inline/take_prize.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, KeyboardButton
-
-
-button_1 = KeyboardButton("Забрать бонус🎁", callback_data="taking_part")
-taking_board = InlineKeyboardMarkup(row_width=1).add(button_1)
