9be70bc5e934a0a8eb0634968a9869f81cb63848
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:36:56 +0300

    Routine functions for message processing

--- functions/message_routins.py
+++ functions/message_routins.py
@@ -10,23 +10,24 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 from config.load_all import bot 
-
+ 
+ 
 class Routins:
     
-    @staticmethod
+    @staticmethod 
     async def routin_callback(callback_query: types.CallbackQuery):
         await callback_query.answer()
-        await bot.edit_message_reply_markup(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id)
+        await bot.edit_message_reply_markup(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
         await bot.send_chat_action(callback_query.from_user.id, types.ChatActions.TYPING)
-        await asyncio.sleep(3)
+        await asyncio.sleep(3) 
 
     @staticmethod
     async def edit_callback(call: types.CallbackQuery):
-        await call.answer()
-        await bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id)
+        await call.answer() 
+        await bot.edit_message_reply_markup(chat_id=call.from_user.id, message_id=call.message.message_id)
 
     @staticmethod
     async def edit_call_text(callback_query: types.CallbackQuery, text):
-        await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id, text=text)
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id, text=text, disable_web_page_preview=True)
         await bot.send_chat_action(callback_query.from_user.id, types.ChatActions.TYPING)
         await asyncio.sleep(3)
