e8781896124058432f366993aa1e431e1f56bda0
Misha <100841904+Mid1i@users.noreply.github.com>
2022-07-26 22:45:30 +0300

    Delete play_board.py

--- menu/inline/play_board.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, KeyboardButton
-
-
-button_1 = KeyboardButton("Я в деле✅", callback_data="i_am_in")
-play_board = InlineKeyboardMarkup(row_width=1).add(button_1)
