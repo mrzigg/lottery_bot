0f944493ea25e38dc37c55b7935a4edad78d6c7d
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-08-04 13:55:36 +0300

    Lottery completion function

--- buttons/win_fuction.py
+++ buttons/win_fuction.py
@@ -13,8 +13,10 @@ import database.users_db as user_db
 import database.lottery_db as lot_db
 import database.tickets_db as ticket_db
 
+from filters.channels import CHANNELS
 from config.load_all import bot
 from database.main_data import customer_id, bot_id, raffle_id
+from variables import customer_phone_number, customer_user_name
 
 
 async def find_winner_function():
@@ -23,11 +25,23 @@ async def find_winner_function():
     winners_sp = list()
     upper_bound = await ticket_db.last_ticket()
     winners_tickets = list() 
-    for i in range(winners_amount):
+    while len(winners_sp) >= winners_amount:
         winner_ticket = randint(1, upper_bound)
-        winner = await ticket_db.find_winner(winner_ticket)
-        winners_tickets.append(winner_ticket)
-        winners_sp.append(winner)
+        winner = await ticket_db.find_winner(winner_ticket) # user_id пользователя, у которого был выбран билет
+        for row in CHANNELS:
+            user_channel_status = await bot.get_chat_member(chat_id=f"@{row}", user_id=winner)
+            main_status = True
+            user_state = False
+            if user_channel_status['status'] == "left":
+                user_state = False
+            else:
+                user_state = True
+            main_status = bool(main_status*user_state)
+            if main_status is True:
+                winners_tickets.append(winner_ticket)
+                winners_sp.append(winner)
+            else:
+                pass
     i = 0
     while True:
         loosers = await user_db.all_winners(1000*i, winners_sp)
@@ -44,6 +58,6 @@ async def find_winner_function():
         i += 1
     for row in winners_sp:
         try:
-            return await bot.send_message(int(row), f"<b>Розыгрыш завершен!</b>\n\nРезультаты розыгрыша здесь:\n👉 https://go.telegiv.com/results/{customer_id}.{bot_id}.{raffle_id}/\n\nСпойлер: Ты выиграл 🥳\n\nСовсем скоро будет новый розыгрыш!", disable_web_page_preview=True)
+            return await bot.send_message(int(row), f"<b>Розыгрыш завершен!</b>\n\nРезультаты розыгрыша здесь:\n👉 https://go.telegiv.com/results/{customer_id}.{bot_id}.{raffle_id}/\n\nСпойлер: Ты выиграл 🥳\n\nСовсем скоро будет новый розыгрыш!\n\nОбратись сюда, чтобы получить приз:\n<b>@{customer_user_name}\n{customer_phone_number}</b>", disable_web_page_preview=True)
         except Exception as e:
             logging.warning(e)
