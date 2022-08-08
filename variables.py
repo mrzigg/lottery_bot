a7720327a7bd7bced560bd027f3480ca9376d3d3
Rustam <96946731+h0riz4n@users.noreply.github.com>
2022-08-08 12:31:56 +0300

    Variables file for telegram bot

--- variables.py
+++ variables.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-API_TOKEN = "2061411546:AAFLLCdjEtYbJLLEZMe-Qr9sp0NaPId1tCU"
-customer_id = 1
-bot_id = 2061411546
-raffle_id = 1
+API_TOKEN = ""
+customer_id = int("")
+bot_id = int("")
+raffle_id = int("")
 start_timestamp = ""
-CHANNELS = ["TELEGIV_TEST", "TEST_TELEGIV_2"]
-end_timestamp = datetime(2022, 7, 25, 10, 00)
-bot_name = "h0riz4nbot"
-customer_user_name = "h0riz4n"
-customer_phone_number = "+79321345676"
+CHANNELS = []
+end_timestamp = datetime()
+bot_name = ""
+customer_user_name = ""
+customer_phone_number = ""
